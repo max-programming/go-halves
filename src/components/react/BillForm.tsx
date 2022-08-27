2efa6a3c6903966063cb975b5fa54a9ac42525f4
max-programming <ui.sabuwala@gmail.com>
2022-08-27 14:54:22 +0530

    Fix build error

--- src/components/react/BillForm.tsx
+++ src/components/react/BillForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
-import { useTimeout } from 'react-use';
+import useTimeout from 'react-use/lib/useTimeout';
 import Confetti from 'react-confetti';
 import BillResult from './BillResult';
 
