072f77770cc516665dbbed95b077dc8f3f1a5aaa
max-programming <ui.sabuwala@gmail.com>
2022-08-27 15:17:28 +0530

    Remove confetti

--- src/components/react/BillForm.tsx
+++ src/components/react/BillForm.tsx
@@ -1,21 +1,15 @@
 import { useState } from 'react';
-import { createPortal } from 'react-dom';
-import useTimeout from 'react-use/lib/useTimeout';
-import Confetti from 'react-confetti';
 import BillResult from './BillResult';
 
 const BillForm = () => {
   const [totalAmount, setTotalAmount] = useState<number>();
   const [noOfPeople, setNoOfPeople] = useState<number>();
   const [splittedAmount, setSplittedAmount] = useState<number>();
-  const [showConfetti, setShowConfetti] = useState(true);
 
   function splitBill() {
     if (!totalAmount || !noOfPeople) return;
     const split = +(totalAmount / noOfPeople).toFixed(2);
     setSplittedAmount(split);
-    setShowConfetti(true);
-    setTimeout(() => setShowConfetti(false), 9000);
   }
 
   return (
@@ -38,21 +32,7 @@ const BillForm = () => {
         SPLIT!
       </button>
       {splittedAmount && <BillResult splittedAmount={splittedAmount} />}
-      {showConfetti && <ConfettiComponent />}
     </div>
-  );
-};
-
-const ConfettiComponent = () => {
-  const [isComplete] = useTimeout(4000);
-
-  return createPortal(
-    <Confetti
-      initialVelocityY={20}
-      initialVelocityX={10}
-      recycle={!isComplete()}
-    />,
-    document.body
   );
 };
 
