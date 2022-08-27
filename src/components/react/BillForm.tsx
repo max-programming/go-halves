a529cfeb72c92f60cb78ef0ed84bc2b9d939230a
max-programming <ui.sabuwala@gmail.com>
2022-08-27 14:38:50 +0530

    Basic build done - Only Responsive

---
+++ src/components/react/BillForm.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import { useTimeout } from 'react-use';
+import Confetti from 'react-confetti';
+import BillResult from './BillResult';
+
+const BillForm = () => {
+  const [totalAmount, setTotalAmount] = useState<number>();
+  const [noOfPeople, setNoOfPeople] = useState<number>();
+  const [splittedAmount, setSplittedAmount] = useState<number>();
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  function splitBill() {
+    if (!totalAmount || !noOfPeople) return;
+    const split = +(totalAmount / noOfPeople).toFixed(2);
+    setSplittedAmount(split);
+    setShowConfetti(true);
+    setTimeout(() => setShowConfetti(false), 9000);
+  }
+
+  return (
+    <div className='flex flex-col justify-center gap-4 items-center mt-5'>
+      <input
+        type='number'
+        className='input input-bordered input-primary w-full text-xl'
+        placeholder='Enter total amount'
+        value={totalAmount}
+        onChange={e => setTotalAmount(e.target.valueAsNumber)}
+      />
+      <input
+        type='number'
+        className='input input-bordered input-primary w-full text-xl'
+        placeholder='Number of people'
+        value={noOfPeople}
+        onChange={e => setNoOfPeople(e.target.valueAsNumber)}
+      />
+      <button className='btn btn-primary w-full text-xl' onClick={splitBill}>
+        SPLIT!
+      </button>
+      {splittedAmount && <BillResult splittedAmount={splittedAmount} />}
+      {showConfetti && <ConfettiComponent />}
+    </div>
+  );
+};
+
+const ConfettiComponent = () => {
+  const [isComplete] = useTimeout(4000);
+
+  return createPortal(
+    <Confetti
+      initialVelocityY={20}
+      initialVelocityX={10}
+      recycle={!isComplete()}
+    />,
+    document.body
+  );
+};
+
+export default BillForm;
---
+++ src/components/react/BillResult.tsx
@@ -0,0 +1,21 @@
+import { motion } from 'framer-motion';
+import { FC } from 'react';
+
+interface Props {
+  splittedAmount: number;
+}
+
+const BillResult: FC<Props> = ({ splittedAmount }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      className='prose'
+    >
+      <h2>Splitted Amount</h2>
+      <h1 className='text-center'>{splittedAmount}</h1>
+    </motion.div>
+  );
+};
+
+export default BillResult;
